1af8bb09149dfeab3a8d74215460bb339743700f
Farid Mammadov <fareedmammadov@gmail.com>
2020-06-14 03:45:33 +0400

    about section completed

--- js/app.js
+++ js/app.js
@@ -29,3 +29,15 @@ dropdownBtn.innerText = defaultLang;
 upperLangs.forEach((lang) => {
   dropdownMenu.innerHTML += `<a class="dropdown-item" href="#">${lang}</a>`;
 });
+
+var slider = tns({
+  container: ".my-slider",
+  items: 1,
+  gutter: 20,
+  controlsContainer: ".gallery-button",
+  nextButton: ".gallery-button",
+  navContainer: ".gallery-nav",
+  responsive: {},
+  mouseDrag: true,
+  preventScrollOnTouch: "auto",
+});
