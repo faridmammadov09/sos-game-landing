5f9ef796b43aa47c633fef007189e9716a728bb5
Farid Mammadov <fareedmammadov@gmail.com>
2020-07-08 12:07:40 +0400

    remove unnecessary comments

--- js/app.js
+++ js/app.js
@@ -1,47 +1,3 @@
-// const langDropdown = document.querySelector(".language-dropdown");
-
-// const dropdownBtn = document.querySelector("#dropdownMenuButton");
-
-// const dropdownMenu = document.querySelector(".dropdown-menu");
-
-// const languages = ["Eng", "Rus", "Fra", "Deu"];
-
-// const upperLangs = languages.map((lang) => {
-//   return lang.toUpperCase();
-// });
-
-// const defaultLang = upperLangs[0];
-// upperLangs.shift();
-
-// dropdownBtn.addEventListener("click", function (e) {
-//   dropdownMenu.classList.toggle("active");
-// });
-
-// langDropdown.addEventListener("click", function (e) {
-//   if (e.target.className === "dropdown-item") {
-//     dropdownBtn.innerText = e.target.innerText;
-//     dropdownMenu.classList.toggle("active");
-//   }
-// });
-
-// dropdownBtn.innerText = defaultLang;
-
-// upperLangs.forEach((lang) => {
-//   dropdownMenu.innerHTML += `<a class="dropdown-item" href="#">${lang}</a>`;
-// });
-
-var slider = tns({
-  container: ".my-slider",
-  items: 1,
-  gutter: 20,
-  controlsContainer: ".gallery-button",
-  nextButton: ".gallery-button",
-  navContainer: ".gallery-nav",
-  responsive: {},
-  mouseDrag: true,
-  preventScrollOnTouch: "auto",
-});
-
 const languageDropdown = document.querySelector(".language-dropdown");
 const dropdownBtn = document.querySelector("#dropdownMenuButton");
 const dropdownMenu = document.querySelector(".dropdown-menu");
@@ -60,4 +16,16 @@ dropdownMenu.addEventListener("click", function (e) {
     e.target.classList.add("active");
     dropdownMenu.classList.toggle("active");
   }
+});
+
+var slider = tns({
+  container: ".my-slider",
+  items: 1,
+  gutter: 20,
+  controlsContainer: ".gallery-button",
+  nextButton: ".gallery-button",
+  navContainer: ".gallery-nav",
+  responsive: {},
+  mouseDrag: true,
+  preventScrollOnTouch: "auto",
 });
